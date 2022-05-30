87f3b97d312c6bebe3324c658e7ac3d75bebe4d1
ali-akkaya <56932500+ali-akkaya@users.noreply.github.com>
2022-05-30 15:12:26 +0300

    changed header, erased for loop

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
+
 # import socket module
 from socket import *
-import sys # In order to terminate the program
+# In order to terminate the program
 serverSocket = socket(AF_INET, SOCK_STREAM)
+
+
 #Prepare a sever socket
 serverPort = 12000
 serverIp = gethostname()
-print(gethostbyname(gethostname()))
-serverSocket.bind((gethostname(), serverPort))
+serverSocket.bind((serverIp, serverPort))
 serverSocket.listen(1)
 while True:
     #Establish the connection
@@ -21,11 +23,12 @@ while True:
         f = open(filename[1:])
         outputdata = f.read()
         #Send one HTTP header line into socket
-        connectionSocket.send("HTTP/1.1 200 OK".encode())
+        connectionSocket.send("HTTP/1.1 200 OK\r\n Content-Type: text/html\n\n".encode())
 
         #Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
+        # for i in range(0, len(outputdata)):
+        #     connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send(outputdata.encode())
         connectionSocket.send("\r\n".encode())
 
         connectionSocket.close()
